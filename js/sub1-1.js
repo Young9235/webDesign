751825af1305dde29eac72ca42f6c6642dd79aa4
Young9235 <inyoung9235@daum.net>
2019-01-05 00:19:44 +0900

    first commit

---
+++ js/sub1-1.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function(){
+		$('#nav > ul > #a').bind('click', function(){
+			$('#image').removeClass().addClass('a');
+		});		
+		$('#nav > ul > #b').bind('click', function(){
+			$('#image').removeClass().addClass('b');
+		});		
+		$('#nav > ul > #c').bind('click', function(){
+			$('#image').removeClass().addClass('c');
+		});	
+		$('#nav > ul > #d').bind('click', function(){
+			$('#image').removeClass().addClass('d');
+		});
+		$('#nav > ul > #e').bind('click', function(){
+			$('#image').removeClass().addClass('e');
+		});
+		$('#nav > ul > #f').bind('click', function(){
+			$('#image').removeClass().addClass('f');
+		});
+		$('#nav > ul > #g').bind('click', function(){
+			$('#image').removeClass().addClass('g');
+		});
+		$('#nav > ul > #h').bind('click', function(){
+			$('#image').removeClass().addClass('h');
+		});
+		$('#nav > ul > #i').bind('click', function(){
+			$('#image').removeClass().addClass('i');
+		});
+		$('#nav > ul > #j').bind('click', function(){
+			$('#image').removeClass().addClass('j');
+		});
+		$('#nav > ul > #k').bind('click', function(){
+			$('#image').removeClass().addClass('k');
+		});
+		$('#nav > ul > #l').bind('click', function(){
+			$('#image').removeClass().addClass('l');
+		});
+});
